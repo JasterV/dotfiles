ec4c757233e6e1401b5670664670e1fba2937f58
Victor Martinez <49537445+JasterV@users.noreply.github.com>
2022-12-29 15:09:57 +0100

    update doom config

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,7 +3,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/Documents/Personal/Notes/first-note.org")))
+ '(org-agenda-files '("~/Documents/Personal/Notes/first-note.org"))
+ '(warning-suppress-log-types
+   '((frameset)
+     (frameset)
+     (frameset)
+     (frameset)
+     (defvaralias)))
+ '(warning-suppress-types
+   '((frameset)
+     (frameset)
+     (frameset)
+     (frameset)
+     (defvaralias))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
