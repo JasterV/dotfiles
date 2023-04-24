440a3c129ec4b341867f1a35f4df1cdcdf93a3e2
Victor Martinez <49537445+JasterV@users.noreply.github.com>
2023-04-25 00:19:07 +0200

    update nvim config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -9,6 +9,13 @@ return {
         rust_analyzer = {},
         gopls = {},
         bashls = {},
+        denols = {
+          root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc"),
+        },
+        tsserver = {
+          root_dir = require("lspconfig.util").root_pattern("package.json"),
+          single_file_support = false,
+        },
       },
     },
   },
