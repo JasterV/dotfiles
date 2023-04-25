0bb2b30e9ed369ad0abd528470e05cc500aa6579
Victor Martinez <49537445+JasterV@users.noreply.github.com>
2023-04-25 10:05:46 +0200

    update nvim config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -9,12 +9,14 @@ return {
         rust_analyzer = {},
         gopls = {},
         bashls = {},
+        -- The configuration below for Deno is no longer required in LazyVim
+        -- I'll leave it here just in case I need it on the future
         denols = {
-          root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc"),
+          -- root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc"),
         },
         tsserver = {
-          root_dir = require("lspconfig.util").root_pattern("package.json"),
-          single_file_support = false,
+          -- root_dir = require("lspconfig.util").root_pattern("package.json"),
+          -- single_file_support = false,
         },
       },
     },
