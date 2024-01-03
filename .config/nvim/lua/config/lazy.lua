269320762519c46232ac93413577be7565abac04
Victor Martinez <49537445+JasterV@users.noreply.github.com>
2024-01-03 16:30:22 +0100

    chore: disable neotree

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -38,6 +38,10 @@ require("lazy").setup({
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
+        -- "matchit",
+        -- "matchparen",
+        "netrwPlugin",
+        "rplugin",
         "tarPlugin",
         "tohtml",
         "tutor",
--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -5,4 +5,9 @@ return {
       colorscheme = "solarized-osaka",
     },
   },
+  -- Disable side files tree
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
 }
